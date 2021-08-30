04147150dc4fab71eed5d7288c4827e6f7c238bb
Paco <paco.mgh@gmail.com>
2021-08-30 09:12:32 -0500

    add practice

---
+++ 03-tipos-datos/e-califica-dia.py
@@ -0,0 +1,6 @@
+#califica tu dia del 1 al 10
+#podemos quitar la conversion a int ya que aqui no la necesitamos
+#solo la usaremos cuando necesitemos hacer un calculo numerico
+calificacion = int(input("Como estuvo tu día? "))
+#convertimos el entero a cadena
+print("El día estuvo de: ", str(calificacion))
