0d86f0f3bacb3c1ae334d4ea65349672f3cf40bc
Paco <paco.mgh@gmail.com>
2021-09-03 13:06:10 -0500

    add range example

---
+++ 03-tipos-datos/rango.py
@@ -0,0 +1,15 @@
+valor = int(input("Ingresa un valor: "))
+valorMinimo = 0
+valorMaximo = 5
+
+dentroDeRango = valor >= valorMinimo and valor <= valorMaximo
+#forma 1
+'''if valor >= 0 and valor<=5:
+    print("El valor esta dentro del rango")
+else:
+    print("El valor esta fuera del rango")'''
+
+if dentroDeRango:
+    print("El valor esta dentro del rango")
+else:
+    print("El valor esta fuera del rango")
