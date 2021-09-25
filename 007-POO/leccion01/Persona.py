738ba62ca414d0236a5aefd777a19af170d29f54
Paco <paco.mgh@gmail.com>
2021-09-24 22:13:55 -0500

    add Persona class file

--- 007-POO/leccion01/Persona.py
+++ 007-POO/leccion01/Persona.py
@@ -15,3 +15,7 @@ print(persona1.nombre)
 print(persona1.apellido)
 print(persona1.edad)
 print(f'El objeto persona1 contiene nombre: {persona1.nombre} {persona1.apellido} y tiene {persona1.edad} anios')
+
+
+persona2 = Persona('Pedro', 'Perez', 19)
+print(f'El objeto persona2 contiene nombre: {persona2.nombre} {persona2.apellido} y tiene {persona2.edad} anios')
